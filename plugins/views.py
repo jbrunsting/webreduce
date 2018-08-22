9a0f1e64b696a7f03c16c361334ec7778cd4e43c
Jacob Brunsting <jacob.brunsting@gmail.com>
2018-08-22 12:36:37 -0400

    Add plugin app

--- plugins/views.py
+++ plugins/views.py
@@ -1,8 +1,14 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+from .models import Plugin
+
 
 @login_required
 def home(request):
-    return render(request, 'plugins/home.html',
-                  {'username': request.user.username})
+    return render(
+        request,
+        'plugins/home.html', {
+            'username': request.user.username,
+            'plugins': Plugin.objects.filter(owners=request.user.id),
+        })
