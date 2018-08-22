53c3d292f35ea1ebedb96970cccb6ec53eab7697
Jacob Brunsting <jacob.brunsting@gmail.com>
2018-08-22 09:20:50 -0400

    Use isort for sorting imports

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render, redirect
-from django.urls import reverse_lazy
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect, render
+from django.urls import reverse_lazy
 
 
 def signup(request):
--- feed/views.py
+++ feed/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 
 
 @login_required
