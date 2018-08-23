7b1700acbce484d1bc394d987d85f9ae8173bbfc
Jacob Brunsting <jacob.brunsting@gmail.com>
2018-08-23 12:54:59 -0400

    Make form for editing plugins

--- plugins/views.py
+++ plugins/views.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
@@ -14,6 +15,51 @@ def home(request):
         })
 
 
+class EditPluginForm(forms.ModelForm):
+    extra_field = forms.CharField(label='Name of Institution') # TODO: Make this edit the owners
+
+    class Meta:
+        model = Plugin
+        fields = [
+            'name',
+            'code',
+            'major_version',
+            'minor_version',
+        ]
+
+    def __init__(self, *args, **kwargs):
+        super(EditPluginForm, self).__init__(*args, **kwargs)
+        self.fields['extra_field'].initial = 'harvard'
+
+
+def get_edit_plugin(request, plugin, error=None):
+    form = EditPluginForm({
+        'name': plugin.name,
+        'owners': plugin.owners.all(),
+        'code': plugin.code,
+        'major_version': plugin.major_version,
+        'minor_version': plugin.minor_version,
+    })
+
+    return render(request, 'plugins/edit.html', {
+        'plugin': plugin,
+        'form': form,
+        'error': error,
+    })
+
+
+def post_edit_plugin(request, plugin_id):
+    form = EditPluginForm(request.POST)
+    plugin = Plugin.objects.get(pk=plugin_id)
+    # TODO: Use django permissions to guard against this
+    if not plugin.owners.filter(pk=request.user.pk).exists():
+        return render(request, 'plugins/not_owner.html', {
+            'plugin_name': plugin.id,
+            'plugin_owners': plugin.owners.all(),
+        })
+    return get_edit_plugin(request, plugin)
+
+
 @login_required
 def edit_plugin(request, plugin_id):
     if not Plugin.objects.filter(pk=plugin_id).exists():
@@ -22,12 +68,14 @@ def edit_plugin(request, plugin_id):
         })
 
     plugin = Plugin.objects.get(pk=plugin_id)
-    if plugin.owners.filter(pk=request.user.pk).exists():
-        return render(request, 'plugins/edit.html', {
-            'plugin': plugin,
-        })
-    else:
+    # TODO: Use django permissions to guard against this
+    if not plugin.owners.filter(pk=request.user.pk).exists():
         return render(request, 'plugins/not_owner.html', {
             'plugin_name': plugin.id,
             'plugin_owners': plugin.owners.all(),
         })
+
+    if request.method == "GET":
+        return get_edit_plugin(request, plugin)
+
+    return post_edit_plugin(request, plugin_id)
