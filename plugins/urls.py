6e73cb1113b75fae598d8dddce262dd7fd7200f2
Jacob Brunsting <jacob.brunsting@gmail.com>
2018-08-23 15:14:07 -0400

    Make plugins publishable

--- plugins/urls.py
+++ plugins/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.home),
     path('edit/<plugin_id>', views.edit_plugin),
+    path('publish/<plugin_id>', views.publish_plugin),
 ]
--- plugins/views.py
+++ plugins/views.py
@@ -1,11 +1,13 @@
 from django import forms
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.views.decorators.http import require_http_methods
 
 from .models import Plugin
 
 
 @login_required
+@require_http_methods(["GET"])
 def home(request):
     return render(
         request,
@@ -16,9 +18,6 @@ def home(request):
 
 
 class EditPluginForm(forms.ModelForm):
-    # TODO: Make this edit the owners
-    extra_field = forms.CharField(label='Name of Institution')
-
     class Meta:
         model = Plugin
         fields = [
@@ -27,10 +26,6 @@ class EditPluginForm(forms.ModelForm):
             'major_version',
             'minor_version',
         ]
-
-    def __init__(self, *args, **kwargs):
-        super(EditPluginForm, self).__init__(*args, **kwargs)
-        self.fields['extra_field'].initial = 'harvard'
 
 
 def get_edit_plugin(request, plugin, error=None):
@@ -68,7 +63,6 @@ def post_edit_plugin(request, plugin_id):
                 error = "Version already published, incriment major or minor version"
             else:
                 plugin.name = form_data['name']
-                # TODO plugin.owners = form_data['owners']
                 plugin.code = form_data['code']
                 plugin.major_version = form_data['major_version']
                 plugin.minor_version = form_data['minor_version']
@@ -76,10 +70,11 @@ def post_edit_plugin(request, plugin_id):
         else:
             new_plugin = Plugin()
             new_plugin.name = form_data['name']
-            # TODO new_plugin.owners = form_data['owners']
             new_plugin.code = form_data['code']
             new_plugin.major_version = form_data['major_version']
             new_plugin.minor_version = form_data['minor_version']
+            new_plugin.save()
+            new_plugin.owners.add(request.user)
             new_plugin.save()
     else:
         error = "Invalid form"
@@ -92,6 +87,7 @@ def post_edit_plugin(request, plugin_id):
 
 
 @login_required
+@require_http_methods(["GET", "POST"])
 def edit_plugin(request, plugin_id):
     if not Plugin.objects.filter(pk=plugin_id).exists():
         return render(request, 'plugins/not_found.html', {
@@ -110,3 +106,20 @@ def edit_plugin(request, plugin_id):
         return get_edit_plugin(request, plugin)
 
     return post_edit_plugin(request, plugin_id)
+
+
+@login_required
+@require_http_methods(["POST"])
+def publish_plugin(request, plugin_id):
+    plugin = Plugin.objects.get(pk=plugin_id)
+    # TODO: Use django permissions to guard against this
+    if not plugin.owners.filter(pk=request.user.pk).exists():
+        return render(request, 'plugins/not_owner.html', {
+            'plugin_name': plugin.id,
+            'plugin_owners': plugin.owners.all(),
+        })
+
+    plugin.published = True
+    plugin.save()
+
+    return redirect('/plugins')
