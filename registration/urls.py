a9f4b9fe51fbca9fb4719e30ec2b5f2cbe02298c
Jacob Brunsting <jacob.brunsting@gmail.com>
2018-09-12 10:22:17 -0400

    Allow for temporary users, let users specify their email address

--- registration/urls.py
+++ registration/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home),
+    path('temporary', views.temporary),
     path('signup', views.signup),
     path('logout', views.logoutUser),
 ]
--- registration/views.py
+++ registration/views.py
@@ -1,3 +1,5 @@
+import uuid
+
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import redirect, render
@@ -12,10 +14,21 @@ def home(request):
     return render(request, 'registration/home.html')
 
 
+def temporary(request):
+    username = str(uuid.uuid4())
+    password = str(uuid.uuid4())
+    user = User(username=username, temporary=True)
+    user.set_password(password)
+    user.save()
+    authenticated = authenticate(username=username, password=password)
+    login(request, authenticated)
+    return redirect('/feed')
+
+
 class SignupForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ("username", )
+        fields = ('username', 'email')
 
 
 def signup(request):
@@ -24,8 +37,10 @@ def signup(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
+            email = form.cleaned_data.get('email')
             raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)
+            user = authenticate(
+                username=username, email=email, password=raw_password)
             login(request, user)
             return redirect('/feed')
     else:
